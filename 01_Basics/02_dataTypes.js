c35299c885d23dc4499244de14d3ccc11b65e78b
Neeraj Wariya <neerajwariya106@gmail.com>
2025-03-03 08:57:56 +0000

    data types

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,37 @@
+"use strict";  // treat aal JS code as newer version
+
+// alert("3 + 3") // we are using nodejs, not browser
+
+// code rediability is good for a good software developer
+/* like console.log(3+3); console.log("neraj")  this is bad way to 
+present the code
+
+code rediability is high
+ */ 
+
+let name = " neeraj" // string datatype
+let age = 18 // number 
+let isLoggedIn = false  // boolean
+
+/*
+DATA TYPE       
+premative data types
+
+console.log(typeof age); use for known the datatype    but null 1 object h
+console.log(typeof undefined);  output undefined
+console.log(typeof null);  output object
+
+1. number => 2 to power of 53 approx
+2. bigint for big number 
+3. string => " name of words"
+4. boolean => true/false
+5. null ==> standalone value  (it is empty value)
+6. undefined =>  not assined value till now
+7. symbol => unique 
+*/
+
+/* OBJECT 
+
+*/
+
+
