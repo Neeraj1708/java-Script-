603b3116c25bab80a04979576c6f038555d8e84f
Neeraj Wariya <neerajwariya106@gmail.com>
2025-03-09 07:03:45 +0000

    ConversionOperations

---
+++ 01_Basics/03_ConversionOperation.js
@@ -0,0 +1,36 @@
+let score = 45
+// 45  output Number
+// "45"  output string
+/*
+if 45 was in double cots "45" it means this was a string 
+*/
+//console.log(typeof score);
+//console.log(typeof (score))
+
+    // typeof use for know what is the type of output like string,no.
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber)  // found NaN not a number
+//console.log(valueInNumber)   
+
+
+    // "45"  => 45
+    //"45abc" => NaN
+    // true => 1;  false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+/*
+if we put 1 => true , 0=> false
+if the empty string => false ,   and  put any thing like
+"Neeraj" => true
+*/
+
+let someNumber = 33 
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
