6d51e078ff7c01410682ea42278dd363a70096ec
Neeraj Wariya <neerajwariya106@gmail.com>
2025-03-09 13:28:08 +0000

    comparison

---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,27 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+//  o/p is true , false
+
+
+
+// Avoid this type of conversion
+    // when we compare different dataType
+    // typeScropt allow nhi krta different datatype ko compare krne ka
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+
+
+// strict check ===
+console.log("2" === 2);
+
+
