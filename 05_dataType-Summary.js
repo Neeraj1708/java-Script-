6d94a14a7fd2749938ae523dda58af99626ecd54
Neeraj Wariya <neerajwariya106@gmail.com>
2025-03-09 13:46:06 +0000

    summary

---
+++ 05_dataType-Summary.js
@@ -0,0 +1,17 @@
+/* categorization of data Types
+1. primitive 
+2. non primitive (Reference)
+*/
+
+// JavaScript is dynamic type language or a static Type language ? 
+// it is dynamic type language, we dont need to declare dataType
+
+/*
+primitive data type
+7 types --> String, Number, Boolean, Undefined, Null, Symbol, BigInt
+*/
+
+/*
+Reference (Non Primitive)
+ --> Array, Objects, Functions 
+*/
